cb25f7b8544bfc004307ee9c67b0590a41148a7e
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-26 22:10:15 +0000

    Challenges - Every and Some methods

--- index.js
+++ index.js
@@ -36,3 +36,83 @@ var someComputerCheck = computers.some((computer) => {
 })
 
 console.log(someComputerCheck);
+
+//Every and Some Syntax
+
+var names = [
+  "Alexandria",
+  "Matthew",
+  "Litty"
+];
+
+var everyNames = names.every((name) => {
+  return name.length > 6;
+})
+
+console.log(everyNames)
+
+var someNames = names.some((name) => {
+  return name.length > 4;
+})
+
+console.log(someNames);
+
+//Every and Some in Practice
+
+function Field(value) {
+  this.value = value;
+}
+
+Field.prototype.validate = function () {
+  return this.value.length > 0;
+}
+var username = new Field("2cool");
+var password = new Field("my_password");
+var birthdate = new Field("12/12/2020")
+
+// console.log(username.validate());
+
+var fields = [ username, password, birthdate ];
+var newFields = fields.every((field) => {
+  return field.validate
+})
+
+if (newFields) {
+  //allow user to submit
+} else {
+  //show an error
+}
+
+console.log(newFields + " Valid Form")
+
+//Challenge 1 - Find Submitted Users
+
+//Given an array of users, return 'true' if every user has submitted a request for
+
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every((user) => {
+  return user.hasSubmitted === true;
+})
+
+console.log(hasSubmitted)
+
+//Challenge 2 - In Progress Network Requests
+
+//Given an array of network objects representing network requests, assign the boolean 'true' to te variable 'inProgress' if any network request has a status of 'pending'
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some((request) => {
+  return request.status = 'pending';
+})
+
+console.log(inProgress)
